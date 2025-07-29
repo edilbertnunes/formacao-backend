a3b587869c830c8bb24514befe6a1461ffeea068
Edilbert Nunes <edilbert.nunes@gmail.com>
2025-07-29 08:04:33 -0300

    feat: add validation to email already exists

--- user-service-api/src/main/java/br/com/edilbert/userserviceapi/repository/UserRepository.java
+++ user-service-api/src/main/java/br/com/edilbert/userserviceapi/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package br.com.edilbert.userserviceapi.repository;
 
 import br.com.edilbert.userserviceapi.entity.User;
+import com.mongodb.client.DistinctIterable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(final String email);
 }
